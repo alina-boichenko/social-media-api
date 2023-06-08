aa00772dc97c898133ec9f070aaec7f7c58ca1f7
Alina Boichenko <ilub1108@gmail.com>
2023-06-08 15:45:01 +0300

    Changed urlpatterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path, include
 from rest_framework import routers
 
 from blog.views import PostViewSet, CommentViewSet, FeedViewSet
@@ -8,8 +7,6 @@ router.register("posts", PostViewSet, basename="posts")
 router.register("comments", CommentViewSet, basename="comments")
 router.register("feed", FeedViewSet, basename="feed")
 
-urlpatterns = [
-    path("", include(router.urls)),
-]
+urlpatterns = router.urls
 
 app_name = "blog"
